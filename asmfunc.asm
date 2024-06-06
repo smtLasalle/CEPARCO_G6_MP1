c3b8aec47567bf1b485b6e6486db3af6289e81ea
smtLasalle <shawne_tumalad@dlsu.edu.ph>
2024-06-06 23:24:28 +0800

    Fixed xmm func

--- asmfunc.asm
+++ asmfunc.asm
@@ -2,7 +2,6 @@
 
 section .data
     sdot dq 0.0 ; result
-    zero dq 0.0 ; always 0
     msg db `%.2lf\n`,0
 
 section .text
@@ -11,87 +10,60 @@ default rel ; for addr reloc
 
 global dotproduct_x86
 global dotproduct_xmm
-global dottest
 extern printf
 
-; Format: rcx = A[] rdx = B[] r8 = ARR_SIZE
+; Format: rcx = ARR_SIZE rdx = A[] r8 = B[]
 
 
 ;x86 implementation
 dotproduct_x86:
-    movq xmm0, qword [zero]    
+    xorpd xmm0, xmm0    
     xor rax, rax 
     
 dotproduct1:
-    cmp rax, r8
-    jge done1
-
-    movsd xmm1, [rcx + rax * 8] 
-    movsd xmm2, [rdx + rax * 8]
+    movsd xmm1, [rdx + rax * 8] 
+    movsd xmm2, [r8 + rax * 8]
     mulsd xmm1, xmm2 
     addsd xmm0, xmm1 
 
     inc rax
-    jmp dotproduct1
+    loop dotproduct1
 
 done1:
-    movq rax, xmm0       ; Move result to rax for return
-    ret
+    xor rax, rax
+    ret ;value is returned in xmm0
 
 
 ;xmm implementation
 dotproduct_xmm:
-    movq xmm0, qword [zero]
-    xor rax, rax 
-
-dotproduct2:
-    cmp rax, r8
-    jge done2
+    mov r9, rcx
     
-    movupd xmm1, [rcx + rax * 8]    
-    movupd xmm2, [rdx + rax * 8] 
+    and r9, 1 ;check if odd number size
+    shr rcx, 1 ;divide by 2
+    xor rax, rax
+    xorpd xmm0, xmm0
+     
+dotproduct2:  
+    movdqu xmm1, [rdx + rax * 8]    
+    movdqu xmm2, [r8 + rax * 8] 
     mulpd xmm1, xmm2
     haddpd xmm1, xmm1
-    
     vaddsd xmm0, xmm1
     
     inc rax
     inc rax
-    jmp dotproduct2
+    loop dotproduct2
+
+end:
+    cmp r9, 0
+    jz done2
+    
+    ;perform 1 more dot product process for last element in odd sized arr
+    movdqu xmm1, [rdx + rax * 8]    
+    movdqu xmm2, [r8 + rax * 8] 
+    mulpd xmm1, xmm2
+    vaddsd xmm0, xmm1      
     
 done2:
     xor rax,rax
     ret ;value is returned in xmm0
-
-dottest:
-    movq xmm0, qword [sdot]
-    
-    shr rcx, 1    
-    xor rax,rax
-loop_start:
-    cmp rax, rcx
-    jge done
-    
-    movdqu xmm1, [rdx]    
-    movdqu xmm2, [r8] 
-    mulpd xmm1, xmm2
-
-    movhlps xmm2, xmm1
-    haddpd xmm1, xmm1
-    addsd xmm0, xmm1
-    
-    add rdx, 16
-    add r8, 16
-    inc rax
-    jmp loop_start
-    
-done:
-    movsd [sdot], xmm0          
-
-    sub rsp, 8*5     
-    mov rcx, msg                
-    mov rdx, [sdot]           
-    call printf                  
-    add rsp, 8*5                
-    xor rax, rax                
-    ret
--- main.c
+++ main.c
@@ -4,15 +4,13 @@
 #include <time.h>
 
 // x86 Function to calculate the dot product of two vectors
-extern double dotproduct_x86(double* a, double* b, size_t n);
-// rcx = a, rdx = b, r8 = n
+extern double dotproduct_x86(long long int n, double* a, double* b);
 
-extern double dotproduct_xmm(double* a, double* b, size_t n);
-
-extern double dottest(int n, double* a, double* b);
+// xmm Function to calculate the dot product of two vectors
+extern double dotproduct_xmm(long long int n, double* a, double* b);
 
 // C Function to calculate the dot product of two vectors
-double dotproduct_C(double* a, double* b, size_t n) {
+double dotproduct_C(long long int n, double* a, double* b) {
     double sum = 0.0;
     for (int i = 0; i < n; i++) {
         sum += a[i] * b[i];
@@ -20,59 +18,61 @@ double dotproduct_C(double* a, double* b, size_t n) {
     return sum;
 }
 
+// Generate random double value between 'min' and 'max'
 double random_double(double min, double max) {
     return min + ((double)rand() / RAND_MAX) * (max - min);
 }
 
-void func_timeavg(double (*operation)(double*, double*, size_t), double* a, double* b, size_t n, int loops){
+void func_timeavg(double (*operation)(long long int, double*, double*), long long int n, double* a, double* b, int loops, double sdot){
     // Variables related to timing
     LARGE_INTEGER freq, start, end;
-    double time_used, sum_times = 0, avg_time, sdot; //sdot is the func output
+    double time_used, sum_times = 0, avg_time;
     QueryPerformanceFrequency(&freq);
 
     for (int i = 0; i < loops; i++) {
         QueryPerformanceCounter(&start); // Record start time
-        sdot = operation(a, b, n);
+        sdot = operation(n, a, b);
         QueryPerformanceCounter(&end); // Record end time
         time_used = (double)(end.QuadPart - start.QuadPart) * 1e3 / freq.QuadPart; // Calculate elapsed time
-        printf("Run %02d: Dot product of A and B is: %.2lf, Time taken: %lf ms\n", i + 1, sdot, time_used);
+        printf("Run %02d Time taken: %lf ms\n", i + 1, time_used);
         sum_times += time_used;
     }
 
     avg_time = sum_times / loops; // Calculate average time
     printf("Average time taken for 30 runs: %lf seconds\n", avg_time);
+    printf("Dot product of A and B is: %.2lf\n", sdot);
 }
 
 int main() {
-    const size_t ARR_SIZE = 8;
+    long long int ARR_SIZE = 1<<20;
     double* a = (double*)malloc(ARR_SIZE * sizeof(double));
     double* b = (double*)malloc(ARR_SIZE * sizeof(double));
 
     srand(time(NULL)); // Random number seed
 
     for (int i = 0; i < ARR_SIZE; i++) {
-        //a[i] = random_double(0.0, 20.0);
-        //b[i] = random_double(0.0, 20.0);
-        a[i] = i + 1;
-        b[i] = i + 1;
+        a[i] = random_double(0.0, 20.0);
+        b[i] = random_double(0.0, 20.0);
+        //a[i] = i + 1;
+        //b[i] = i + 1;
     }
 
-    //dotproduct_xmm(a, b, ARR_SIZE);
-
     int loops = 30;
-
-    //dottest(6, a, b);
+    double* sdot = (double*)malloc(4 * sizeof(double));
 
     // Run the dot product functions 30 times and measure the time
 
     // C
-    func_timeavg(dotproduct_C, a, b, 7, loops);
+    printf("\nDot product function in C:\n");
+    func_timeavg(dotproduct_C, ARR_SIZE, a, b, loops, sdot[0]);
 
     // X86
-    func_timeavg(dotproduct_x86, a, b, 7, loops);
+    printf("\nDot product function in x86:\n");
+    func_timeavg(dotproduct_x86, ARR_SIZE, a, b, loops, sdot[1]);
 
     // XMM
-    func_timeavg(dotproduct_xmm, a, b, 7, loops);
+    printf("\nDot product function in xmm:\n");
+    func_timeavg(dotproduct_xmm, ARR_SIZE, a, b, loops, sdot[2]);
 
     return 0;
 }
