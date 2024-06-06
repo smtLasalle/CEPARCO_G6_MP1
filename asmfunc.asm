9076f8eb2d008e5da5757dcab39f80620065c70c
smtLasalle <shawne_tumalad@dlsu.edu.ph>
2024-06-07 01:25:30 +0800

    Added ymm function and improved C file output

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,16 +1,12 @@
 ;assembly
 
-section .data
-    sdot dq 0.0 ; result
-    msg db `%.2lf\n`,0
-
 section .text
 bits 64
 default rel ; for addr reloc
 
 global dotproduct_x86
 global dotproduct_xmm
-extern printf
+global dotproduct_ymm
 
 ; Format: rcx = ARR_SIZE rdx = A[] r8 = B[]
 
@@ -18,7 +14,7 @@ extern printf
 ;x86 implementation
 dotproduct_x86:
     xorpd xmm0, xmm0    
-    xor rax, rax 
+    xor rax, rax
     
 dotproduct1:
     movsd xmm1, [rdx + rax * 8] 
@@ -29,7 +25,7 @@ dotproduct1:
     inc rax
     loop dotproduct1
 
-done1:
+end1:
     xor rax, rax
     ret ;value is returned in xmm0
 
@@ -48,22 +44,71 @@ dotproduct2:
     movdqu xmm2, [r8 + rax * 8] 
     mulpd xmm1, xmm2
     haddpd xmm1, xmm1
-    vaddsd xmm0, xmm1
+    vaddpd xmm0, xmm1
     
-    inc rax
-    inc rax
+    add rax, 2
     loop dotproduct2
 
-end:
+xbd1:
     cmp r9, 0
-    jz done2
+    jz end2
     
     ;perform 1 more dot product process for last element in odd sized arr
     movdqu xmm1, [rdx + rax * 8]    
     movdqu xmm2, [r8 + rax * 8] 
     mulpd xmm1, xmm2
-    vaddsd xmm0, xmm1      
+    vaddpd xmm0, xmm1      
     
-done2:
+end2:
     xor rax,rax
+    ret ;value is returned in xmm0
+
+
+;ymm implementation
+dotproduct_ymm:
+    vxorpd ymm0, ymm0, ymm0
+    mov r12, rcx
+    
+    and r12, 3 ;indicates number of elems outside boundary(0-3)
+    shr rcx, 2 ;divide by 4
+    xor rax, rax 
+
+dotproduct3:
+    vmovupd ymm1, [rdx + rax * 8]
+    vmovupd ymm2, [r8 + rax * 8]
+    vmulpd ymm1, ymm1, ymm2
+    vaddpd ymm0, ymm0, ymm1
+
+    add rax, 4
+    loop dotproduct3
+
+done3:
+    vextractf128 xmm1, ymm0, 1
+    vaddpd xmm0, xmm0, xmm1
+    vhaddpd xmm0, xmm0, xmm0
+
+ybd2:
+    cmp r12, 2 ;checks if there are 2 or more boundary elements
+    jnge ybd1
+    
+    movdqu xmm1, [rdx + rax * 8]    
+    movdqu xmm2, [r8 + rax * 8] 
+    mulpd xmm1, xmm2
+    haddpd xmm1, xmm1
+    vaddpd xmm0, xmm1   
+    
+    add rax, 2     
+    sub r12, 2
+    
+ybd1:
+    cmp r12, 1 ;checks if there is 1 boundary element left
+    jl end3
+    
+    movdqu xmm1, [rdx + rax * 8]    
+    movdqu xmm2, [r8 + rax * 8] 
+    mulpd xmm1, xmm2
+    vaddpd xmm0, xmm1
+
+end3:
+    xor rax, rax
     ret ;value is returned in xmm0
--- main.c
+++ main.c
@@ -9,6 +9,9 @@ extern double dotproduct_x86(long long int n, double* a, double* b);
 // xmm Function to calculate the dot product of two vectors
 extern double dotproduct_xmm(long long int n, double* a, double* b);
 
+// xmm Function to calculate the dot product of two vectors
+extern double dotproduct_ymm(long long int n, double* a, double* b);
+
 // C Function to calculate the dot product of two vectors
 double dotproduct_C(long long int n, double* a, double* b) {
     double sum = 0.0;
@@ -23,24 +26,25 @@ double random_double(double min, double max) {
     return min + ((double)rand() / RAND_MAX) * (max - min);
 }
 
-void func_timeavg(double (*operation)(long long int, double*, double*), long long int n, double* a, double* b, int loops, double sdot){
+// Time averaging function
+void func_timeavg(double (*operation)(long long int, double*, double*), long long int n, double* a, double* b, int loops, double* sdot, double* avgtime){
     // Variables related to timing
     LARGE_INTEGER freq, start, end;
-    double time_used, sum_times = 0, avg_time;
+    double time_used, sum_times = 0;
     QueryPerformanceFrequency(&freq);
 
     for (int i = 0; i < loops; i++) {
         QueryPerformanceCounter(&start); // Record start time
-        sdot = operation(n, a, b);
+        *sdot = operation(n, a, b);
         QueryPerformanceCounter(&end); // Record end time
         time_used = (double)(end.QuadPart - start.QuadPart) * 1e3 / freq.QuadPart; // Calculate elapsed time
         printf("Run %02d Time taken: %lf ms\n", i + 1, time_used);
         sum_times += time_used;
     }
 
-    avg_time = sum_times / loops; // Calculate average time
-    printf("Average time taken for 30 runs: %lf seconds\n", avg_time);
-    printf("Dot product of A and B is: %.2lf\n", sdot);
+    *avgtime = sum_times / loops; // Calculate average time
+    printf("Average time taken for 30 runs: %lf ms\n", *avgtime);
+    printf("Dot product of A and B is: %.2lf\n", *sdot);
 }
 
 int main() {
@@ -58,21 +62,31 @@ int main() {
     }
 
     int loops = 30;
-    double* sdot = (double*)malloc(4 * sizeof(double));
+    double sdot[4], avgtimes[4];
 
     // Run the dot product functions 30 times and measure the time
 
     // C
     printf("\nDot product function in C:\n");
-    func_timeavg(dotproduct_C, ARR_SIZE, a, b, loops, sdot[0]);
+    func_timeavg(dotproduct_C, ARR_SIZE, a, b, loops, &sdot[0], &avgtimes[0]);
 
     // X86
     printf("\nDot product function in x86:\n");
-    func_timeavg(dotproduct_x86, ARR_SIZE, a, b, loops, sdot[1]);
+    func_timeavg(dotproduct_x86, ARR_SIZE, a, b, loops, &sdot[1], &avgtimes[1]);
 
     // XMM
     printf("\nDot product function in xmm:\n");
-    func_timeavg(dotproduct_xmm, ARR_SIZE, a, b, loops, sdot[2]);
+    func_timeavg(dotproduct_xmm, ARR_SIZE, a, b, loops, &sdot[2], &avgtimes[2]);
+
+    // YMM
+    printf("\nDot product function in ymm:\n");
+    func_timeavg(dotproduct_ymm, ARR_SIZE, a, b, loops, &sdot[3], &avgtimes[3]);
+
+    printf("\nDot product summary:\n");
+    printf("  C function answer = %.2lf | Average time taken: %lf ms\n", sdot[0], avgtimes[0]);
+    printf("x86 function answer = %.2lf | Average time taken: %lf ms\n", sdot[1], avgtimes[1]);
+    printf("xmm function answer = %.2lf | Average time taken: %lf ms\n", sdot[2], avgtimes[2]);
+    printf("ymm function answer = %.2lf | Average time taken: %lf ms\n", sdot[3], avgtimes[3]);
 
     return 0;
 }
